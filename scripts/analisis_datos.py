bd2b4cc9f58fbddc9b6f24b43891d0e7bee243d4
matiasbozzo <matiasbozzo5@email.com>
2026-05-30 23:40:36 +0000

    SCRUM-6: implementar calculos basicos para resumen de ventas

--- scripts/analisis_datos.py
+++ scripts/analisis_datos.py
@@ -3,3 +3,18 @@ import matplotlib.pyplot as plt
 
 #Cargamos datos
 ventas = pd.read_csv("datos/dataset.csv")
+
+#### RESUMEN DE VENTAS ####
+
+#Calculamos ventas totales, maximas , minimas y promedio de ventas
+ventas_totales = ventas["sales_amount"].sum()
+venta_maxima = ventas["sales_amount"].max()
+venta_minima = ventas["sales_amount"].min()
+promedio_ventas = ventas["sales_amount"].mean()
+
+#Mostramos los calculos
+print("\n----- RESUMEN DE VENTAS -----")
+print("Ventas totales:", ventas_totales)
+print("Venta máxima:", venta_maxima)
+print("Venta mínima:", venta_minima)
+print("Promedio:", round(promedio_ventas, 2))
