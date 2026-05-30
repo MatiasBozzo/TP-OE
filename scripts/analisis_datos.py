82e82aa2a8e49f0619300942396530ceb78bb334
matiasbozzo <matiasbozzo5@email.com>
2026-05-30 23:42:26 +0000

    SCRUM-6: implementar calculos de ventas por mes

--- scripts/analisis_datos.py
+++ scripts/analisis_datos.py
@@ -18,3 +18,22 @@ print("Ventas totales:", ventas_totales)
 print("Venta máxima:", venta_maxima)
 print("Venta mínima:", venta_minima)
 print("Promedio:", round(promedio_ventas, 2))
+
+#### VENTAS POR MES ####
+
+#Convertimos las fechas al formato fecha (en el csv vienen como strings por defecto)
+ventas["sales_date"] = pd.to_datetime(ventas["sales_date"])
+
+#Creamos una columna con año y mes
+ventas["mes"] = ventas["sales_date"].dt.strftime("%Y-%m")
+
+#Agrupamos ventas por mes
+ventas_por_mes = (
+    ventas.groupby("mes")["sales_amount"]
+    .sum()
+    .reset_index()
+)
+
+#Mostramos las ventas por mes
+print("\nVentas por mes:")
+print(ventas_por_mes)
